0cc26df673d75508bb6e5720a1c901a14c0d8f08
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2020-12-26 21:30:57 -0500

    Update config.py

--- src/openvtuber/client/config.py
+++ src/openvtuber/client/config.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 
 @dataclass
 class Configuration:
-    ip_address: str = "0.0.0.0"
+    ip_address: str = "127.0.0.1"
     port: int = 17701
-    static_files_dir: str = "openvtuber-client/build"
+    static_files_dir: str = "../openvtuber-client/build"
