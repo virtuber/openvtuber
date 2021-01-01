a815eef58c3c10043214307c09751a3d2597fb33
Mingde Yin <54449457+itchono@users.noreply.github.com>
2021-01-01 17:56:26 -0500

    Add WS
    Currently, captures video locally, and sends completed control data via WS to the client

--- src/openvtuber/stream/stream.py
+++ src/openvtuber/stream/stream.py
@@ -1,6 +1,9 @@
+from config import Configuration as config
 from rx import Observable, interval, operators as op
 from cv2 import VideoCapture
+import websockets
 
+ws = websockets.connect(f"ws://localhost:42069")
 
 def cv_videocapture(v: VideoCapture) -> Observable:
     FPS = 30
@@ -8,3 +11,7 @@ def cv_videocapture(v: VideoCapture) -> Observable:
         op.map(lambda _: v.read()),
         op.filter(lambda data: data[0]),
         op.map(lambda data: data[1]))
+
+async def send_data_coro(data):
+    # Called every time the control stream produces a finished set of data to export
+    await ws.send(data)
