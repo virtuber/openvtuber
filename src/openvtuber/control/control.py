5477ecc28478b57864f5d4bcd32ffa8927eb226a
Nuha Sahraoui <nuha.sahraoui@gmail.com>
2021-01-04 17:20:10 -0500

    fixed formatting error yet again

--- src/openvtuber/control/control.py
+++ src/openvtuber/control/control.py
@@ -1,6 +1,6 @@
 def ml_to_vrm_state(*args):
     roll, pitch, yaw, left_eye_aspect_ratio, right_eye_aspect_ratio, mouth_aspect_ratio, \
-    mouth_distance, left_iris, right_iris = args[0]
+        mouth_distance, left_iris, right_iris = args[0]
     x_l, y_l, ll, lu = left_iris
     x_r, y_r, rl, ru = right_iris
     blinkLeftValue = 1
