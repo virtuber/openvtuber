3887d53c65aebdc0998f4065b1fb9aed3ab50bf1
Sean D'Souza <sdammobubbles@gmail.com>
2021-01-04 22:10:08 -0500

    Improve reliability of vrm functions

--- openvtuber-client/src/vrm/VrmFunctions.ts
+++ openvtuber-client/src/vrm/VrmFunctions.ts
@@ -1,7 +1,8 @@
-import { Vector3 } from 'three';
 import THREEVRM from '@pixiv/three-vrm';
-import type { VRM } from '@pixiv/three-vrm';
-import type { VrmState } from '../types';
+import {Vector3} from 'three';
+
+import type {VRM} from '@pixiv/three-vrm';
+import type {VrmState} from '../types';
 
 /**
  * Updates VRM with new state.
@@ -9,46 +10,55 @@ import type { VrmState } from '../types';
 export const updateVrm = (vrm: VRM, state: VrmState, delta: number): void => {
   if (vrm.humanoid) {
     const chest = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.Chest,
+        THREEVRM.VRMSchema.HumanoidBoneName.Chest,
     );
     const upperChest = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.UpperChest,
+        THREEVRM.VRMSchema.HumanoidBoneName.UpperChest,
     );
     const leftShoulder = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.LeftShoulder,
+        THREEVRM.VRMSchema.HumanoidBoneName.LeftShoulder,
     );
     const rightShoulder = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.RightShoulder,
+        THREEVRM.VRMSchema.HumanoidBoneName.RightShoulder,
     );
     const neck = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.Neck,
-    );
-    const jaw = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.Jaw,
+        THREEVRM.VRMSchema.HumanoidBoneName.Neck,
     );
     const head = vrm.humanoid.getBoneNode(
-      THREEVRM.VRMSchema.HumanoidBoneName.Head,
+        THREEVRM.VRMSchema.HumanoidBoneName.Head,
     );
-    if (
-      chest &&
-      upperChest &&
-      leftShoulder &&
-      rightShoulder &&
-      neck &&
-      head
-    ) {
+    const jaw = vrm.humanoid.getBoneNode(
+        THREEVRM.VRMSchema.HumanoidBoneName.Jaw,
+    );
+    if (head) {
       head.rotation.x = state.headRotationX;
       head.rotation.y = state.headRotationY;
+    }
 
+    if (jaw) {
+      jaw.rotation.x = state.jawRotationX;
+      jaw.rotation.y = state.jawRotationY;
+    }
+    if (neck) {
       neck.rotation.x = state.neckRotationX;
       neck.rotation.y = state.neckRotationY;
+    }
 
+    if (leftShoulder) {
       leftShoulder.rotation.y = Math.PI / 3;
       leftShoulder.rotation.x = Math.PI / 2;
+    }
+    if (rightShoulder) {
       rightShoulder.rotation.y = -Math.PI / 3;
       rightShoulder.rotation.x = Math.PI / 2;
+    }
 
+    if (chest) {
+      chest.rotation.x = 0;
       chest.rotation.y = 0;
+    }
+    if (upperChest) {
+      upperChest.rotation.x = 0;
       upperChest.rotation.y = 0;
     }
   }
@@ -57,7 +67,8 @@ export const updateVrm = (vrm: VRM, state: VrmState, delta: number): void => {
     vrm.blendShapeProxy.setValue('blink_r', state.blinkRightValue);
     vrm.blendShapeProxy.setValue('fun', 0.5);
   }
-  if (vrm.lookAt)
+  if (vrm.lookAt) {
     vrm.lookAt.lookAt(new Vector3(state.lookAtX, state.lookAtY, state.lookAtZ));
+  }
   vrm.update(delta);
 };
