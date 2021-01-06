90147e70afc27d1a01ae4f38e871e9585dd2e71f
Nuha Sahraoui <nuha.sahraoui@gmail.com>
2021-01-06 17:32:01 -0500

    fixed eyes, disabled head rotation for testing

--- src/openvtuber/control/control.py
+++ src/openvtuber/control/control.py
@@ -1,6 +1,6 @@
 def set_blink(eye_aspect_ratio):
     # average human eye aspect ratio is assumed to be 0.25
-    return (eye_aspect_ratio / 0.25)
+    return (min(eye_aspect_ratio/0.35, 1))
 
 
 def set_gaze_left(left_iris, right_iris):
@@ -45,13 +45,13 @@ def ml_to_vrm_state(*args):
     neckRotationX = 0
     neckRotationY = 0
 
-    blinkLeftValue = set_blink(eye_aspect_ratio_left)
-    blinkRightValue = set_blink(eye_aspect_ratio_right)
+    blinkLeftValue = 1 - set_blink(eye_aspect_ratio_left)
+    blinkRightValue = 1 - set_blink(eye_aspect_ratio_right)
     lookAtX = set_gaze_left(left_iris, right_iris)
     lookAtY = set_gaze_up(left_iris, right_iris)
-    headRotationX = set_head_rotation_X(yaw)
-    headRotationY = set_head_rotation_Y(roll)
-    jawRotationY = set_jaw_rotation_Y(mouth_distance, mouth_aspect_ratio)
+    # headRotationX = set_head_rotation_X(yaw)
+    # headRotationY = set_head_rotation_Y(roll)
+    # jawRotationY = set_jaw_rotation_Y(mouth_distance, mouth_aspect_ratio)
 
     return (blinkLeftValue, blinkRightValue, headRotationX, headRotationY, jawRotationX,
             jawRotationY, lookAtX, lookAtY, lookAtZ, neckRotationX, neckRotationY)
