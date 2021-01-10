1f49eb5083dcbf563c117a8617f7b0270c685ce4
Victor Wang <victorwang2001@gmail.com>
2021-01-10 00:06:13 -0500

    linear extrapolate

--- openvtuber-server/src/openvtuber/ml/ml.py
+++ openvtuber-server/src/openvtuber/ml/ml.py
@@ -3,6 +3,8 @@ import dlib
 import numpy as np
 from openvtuber import utils
 from .poseEstimator import PoseEstimator
+from .mlUtils import extrapolate
+from collections import deque
 
 
 def infer(image):
@@ -22,7 +24,42 @@ class Inference:
         path = "openvtuber/assets/shape_predictor_68_face_landmarks.dat"
         self.dlib_model_path = str(self.root.joinpath(path))
         self.shape_predictor = dlib.shape_predictor(self.dlib_model_path)
+
+
+        # store 5 vals, 0 (front) is most recent, -1 (end) is furthest away
+        self.val_hist = deque([])
         pass
+
+    def add_val_to_hist(self, val):
+        self.val_hist.appendleft(val)
+        if len(self.val_hist) > 5:
+            self.val_hist.pop()
+
+    def get_from_val_hist(self, amount):
+        return list(self.val_hist)[0 : amount]
+
+    def extrapolate_last_two(self):
+        last_two = self.get_from_val_hist(2)
+
+        # can't use iter here becaues of nested values for iris
+        if last_two[0] is None or last_two[1] is None:
+            return None
+
+        roll1, pitch1, yaw1, ear_left1, ear_right1, mar1, mdst1, left_iris1, right_iris1 = last_two[0]
+        roll2, pitch2, yaw2, ear_left2, ear_right2, mar2, mdst2, left_iris2, right_iris2 = last_two[1]
+
+        roll_e = extrapolate(roll1[0], roll2[0])
+        pitch_e = extrapolate(pitch1[0], pitch2[0])
+        yaw_e = extrapolate(yaw1[0], yaw2[0])
+        ear_left_e = extrapolate(ear_left1, ear_left2)
+        ear_right_e = extrapolate(ear_right1, ear_right2)
+        mar_e = extrapolate(mar1, mar2)
+        mdst_e = extrapolate(mdst1, mdst2)
+        left_iris_e = [extrapolate(z) for z in zip(left_iris1, left_iris2)]
+        right_iris_e = [extrapolate(z) for z in zip(right_iris1, right_iris2)]
+        
+        out = (roll_e, pitch_e, yaw_e, ear_left_e, ear_right_e, mar_e, mdst_e, left_iris_e, right_iris_e)
+        return out
 
     def get_face(self, detector, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -151,6 +188,7 @@ class Inference:
                     ps_stb.update([value])
                     steady_poes.append(ps_stb.state[0])
             """
+
             # everything is in degrees
             roll = np.clip(-(180+np.degrees(pose[2])), -50, 50)
             pitch = np.clip(-(np.degrees(pose[1])) - self.camera_angle, -40, 40)
@@ -169,6 +207,10 @@ class Inference:
             left_iris = [x_l, y_l, ll, lu]
             right_iris = [y_r, y_r, rl, ru]
 
-            return (roll, pitch, yaw, ear_left, ear_right, mar, mdst, left_iris, right_iris)
+            out = (roll, pitch, yaw, ear_left, ear_right, mar, mdst, left_iris, right_iris)
+
+            self.add_val_to_hist(out)
+            return out
         else:
+            self.add_val_to_hist(None)
             return None
