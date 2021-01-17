6692b8f6e0a583b2b2609d0a671eede1d2900926
Victor Wang <victorwang2001@gmail.com>
2021-01-17 11:41:45 -0500

    it's smooth as michael jackson now

--- openvtuber-server/src/openvtuber/stream/stream.py
+++ openvtuber-server/src/openvtuber/stream/stream.py
@@ -3,44 +3,44 @@ from cv2 import VideoCapture
 from openvtuber.protobufs import VrmStateMessage
 import asyncio
 
-ws = None
-
-queue = None
-# Bootleg queue of one variable
-# This is basically a stream except way worse
-
-
-def cv_videocapture(v: VideoCapture) -> Observable:
-    FPS = 30
-    return interval(1 / FPS).pipe(
-        op.map(lambda _: v.read()),
-        op.filter(lambda data: data[0]),
-        op.map(lambda data: data[1]))
+class Stream():
+    def __init__(self):
+        self.queue = None
+    # Bootleg queue of one variable
+    # This is basically a stream except way worse
 
 
-def queue_control_data(data):
-    global queue
-    queue = data
+    def cv_videocapture(self, v: VideoCapture) -> Observable:
+        FPS = 30
+        return interval(1 / FPS).pipe(
+            op.map(lambda _: v.read()),
+            op.filter(lambda data: data[0]),
+            op.map(lambda data: data[1]))
 
 
-async def websocket_handler(websocket, path):
-    # called when the client connects to the server
-    while True:
-        await asyncio.sleep(0.1)  # 10 FPS
-        if queue:
-            # extra check to make sure queue is nonempty
-            encoded_data = control_to_protobuf(queue)
-            print(encoded_data)
-            # Remove this once we're happy with data egress
-            try:
-                await websocket.send(encoded_data)
-            except Exception:
-                break  # upon termination of connection, break loop
+    def queue_control_data(self, data):
+        # global queue
+        self.queue = data
 
 
-def control_to_protobuf(args) -> bytes:
-    vsm = VrmStateMessage()
-    vsm.blinkLeftValue, vsm.blinkRightValue, vsm.headRotationX, vsm.headRotationY, \
-        vsm.jawRotationX, vsm.jawRotationY, vsm.lookAtX, vsm.lookAtY, vsm.lookAtZ, \
-        vsm.neckRotationX, vsm.neckRotationY = map(float, args)
-    return vsm.SerializeToString()
+    async def websocket_handler(self, websocket, path):
+        # called when the client connects to the server
+        while True:
+            await asyncio.sleep(0.0333)  # 10 FPS
+            if self.queue:
+                # extra check to make sure queue is nonempty
+                encoded_data = self.control_to_protobuf(self.queue)
+                print(encoded_data)
+                # Remove this once we're happy with data egress
+                try:
+                    await websocket.send(encoded_data)
+                except Exception:
+                    break  # upon termination of connection, break loop
+
+
+    def control_to_protobuf(self, args) -> bytes:
+        vsm = VrmStateMessage()
+        vsm.blinkLeftValue, vsm.blinkRightValue, vsm.headRotationX, vsm.headRotationY, \
+            vsm.jawRotationX, vsm.jawRotationY, vsm.lookAtX, vsm.lookAtY, vsm.lookAtZ, \
+            vsm.neckRotationX, vsm.neckRotationY = map(float, args)
+        return vsm.SerializeToString()
