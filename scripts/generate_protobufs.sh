dc5c540a17f91a0690c054c17b5bf35cfcd44af8
Sean D'Souza <sdammobubbles@gmail.com>
2021-01-17 19:57:10 -0500

    Switch cjs to esm converter

--- scripts/generate_protobufs.sh
+++ scripts/generate_protobufs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_ROOT=$(realpath "$(dirname $0)/..")
 PBJS_PATH="$PROJECT_ROOT/openvtuber-client/node_modules/protobufjs/bin"
-CJS_TO_ES6_PATH="$PROJECT_ROOT/openvtuber-client/node_modules/cjs-to-es6"
+CJS_TO_ESM_PATH="$PROJECT_ROOT/openvtuber-client/scripts"
 JS_OUT_DIR="$PROJECT_ROOT/openvtuber-client/src/protobufs"
 PY_OUT_DIR="$PROJECT_ROOT/openvtuber-server/src/openvtuber/protobufs"
 PROTO_PATH="$PROJECT_ROOT/openvtuber-server/src/openvtuber/protobufs"
@@ -14,4 +14,4 @@ protoc \
 ${PBJS_PATH}/pbjs ${PROTO_PATH}/VrmState.proto -t static-module -w commonjs -o ${JS_OUT_DIR}/VrmStateMessage.js
 ${PBJS_PATH}/pbts ${JS_OUT_DIR}/VrmStateMessage.js -o ${JS_OUT_DIR}/VrmStateMessage.d.ts
 
-${CJS_TO_ES6_PATH}/index.js ${JS_OUT_DIR}/VrmStateMessage.js
+${CJS_TO_ESM_PATH}/cjs-to-esm.js
