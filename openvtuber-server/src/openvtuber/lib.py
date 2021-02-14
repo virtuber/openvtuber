fd0abb4de1c26b9e9c3d3d9ce972d80aa72bb827
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2021-02-13 20:20:14 -0500

    improve config reading

--- openvtuber-server/src/openvtuber/lib.py
+++ openvtuber-server/src/openvtuber/lib.py
@@ -1,4 +1,3 @@
-from _typeshed import AnyPath
 import cv2
 from rx import operators as op
 from openvtuber import stream, ml, web, control, utils
@@ -37,7 +36,7 @@ def send_data(data):
 @click.option('--cam', required=False, type=str, help='enable cam output', default="false")
 @click.option('--linear_extrap', required=False, type=str,
               help='uses linear extrapolation to speed up ml module', default="false")
-@click.option('--config', required=False, type=AnyPath,
+@click.option('--config_path', required=False, type=str,
               help='filepath to config file for app', default="")
 def main(debug, cam, linear_extrap, config_path, graphtrim):
     if debug != "false" and debug != "true":
@@ -72,8 +71,7 @@ e.g. --linear_extrap=true or --linear_extrap=false")
     else:
         linear_extrap = (linear_extrap == "true")
     
-    
-    config.read_config(config_path)
+    config = config.read_config(config_path)
     utils.get_assets()
     inference = ml.Inference(linear_extrap)
     web_thread = threading.Thread(target=web.run_web_server)
