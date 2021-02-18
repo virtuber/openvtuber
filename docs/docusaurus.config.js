d759e2c1b0712b5d9b5df6d3e311fb1a8b290f8d
Sean D'Souza <sdammobubbles@gmail.com>
2021-02-17 21:26:43 -0500

    Update url to /

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "openvtuber docs",
   tagline: "openvtuber developer documentation",
   url: "https://virtuber.github.io",
-  baseUrl: "/docs/",
+  baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
