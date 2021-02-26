b56dd36617055815c7980df711f052503d4b3116
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2021-02-26 17:14:46 -0500

    Update config.py

--- openvtuber-server/src/openvtuber/web/config.py
+++ openvtuber-server/src/openvtuber/web/config.py
@@ -1,15 +1,10 @@
 from dataclasses import dataclass
+from ..config import Configuration as config
 
 @dataclass
 class Configuration:
-    ip_address: str
-    port: int 
-    static_files_dir: str
-    ws_port: int
-
-    def __init__(self, ip_address, port, static_files_dir, ws_port):
-        self.ip_address = ip_address
-        self.port = port
-        self.static_files_dir = static_files_dir
-        self.ws_port = ws_port
+    ip_address: str = config.web.ip_address
+    port: int = config.web.port
+    static_files_dir: str = config.web.static_files_dir
+    ws_port: int = config.web.ws_port
 
